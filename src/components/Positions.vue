78adee16865d748afc2327a0a2272b5fb8c00b17
jdkolassa <jdkolassa@gmail.com>
2021-12-22 16:17:11 -0500

    Originally I planned to use the LinkedIn API to grab all my jobs; however, that looks like a massive pain, so instead I am putting the data into a local json file and will read from there.

--- src/components/Positions.vue
+++ src/components/Positions.vue
@@ -1,23 +1,30 @@
 <!-- This component will render a slider of my previous employment history-->
 <script setup>
 import { Carousel3d, Slide } from 'vue-carousel-3d';
+import positions from '../data/positions.json';
+export default {
+    // custom option
+    positions: positions
+}
 </script>
 
 <template>
 
-<carousel-3d :count="slides.length">
-    <slide v-for="(slide, i) in slides" :index="i" class="rounded-2xl bg-pink-100">
-      <h1>{{slide.title}}</h1>
+<carousel-3d :count="positions.length">
+    <slide v-for="(position, i) in positions" :index="i" class="rounded-2xl bg-pink-100">
+      <h1>{{position.title}}</h1>
       <div class="md:container mx-auto">
         <div class="jobcard float-left">
-            <img src={{slide.logo}} class="">
-            <h4 class="font-serif">{{slide.employer}}</h4>
+            <img src={{position.logo}} class="">
+            <h4 class="font-serif">{{position.employer}}</h4>
             <ul class="list-none list-outside">
-                <li class="font-sans">{{slide.dates}}</li>
-                <li class="font-sans">{{slide.location}}</li>
+                <li class="font-sans">{{position.dates}}</li>
+                <li class="font-sans">{{position.location}}</li>
             </ul>
         </div>
-        <p>{{slide.details}}</p> <!-- the bullet list of what I did, should wrap around the employer section if long -->
+        <ul class="list-disc list-outside"><!-- the bullet list of what I did, should wrap around the employer section if long -->
+            <li v-for="position.details.item in position.details">{{item}}</li>
+        </ul>
           
       </div>
     </slide>
