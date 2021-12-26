0d762d774486018ad74ff296ecd8b678a9216ebe
jdkolassa <jdkolassa@gmail.com>
2021-12-26 16:42:30 -0500

    Scaffolding out the roles section of my site

--- src/components/Hats.vue
+++ src/components/Hats.vue
@@ -1,1 +1,19 @@
 <!-- This component will be skills and roles -->
+<script setup>
+import hats from '../data/hats.json';
+export default {
+    hats: hats
+}
+</script>
+
+<template>
+    <div>
+        <div v-for="hat in $options.hats">
+            <i class="fas {{hat.icon}}"></i>
+            <h2>{{hat.title}}</h2>
+            <ul v-for="item in hat.details">
+                
+            </ul>
+        </div>
+    </div>
+</template>
