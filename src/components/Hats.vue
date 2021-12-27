067ccfe09bced29c4040e74d831f859f57f825de
jdkolassa <jdkolassa@gmail.com>
2021-12-27 10:07:48 -0500

    Added CSS classes and filled out more of this

--- src/components/Hats.vue
+++ src/components/Hats.vue
@@ -8,11 +8,11 @@ export default {
 
 <template>
     <div>
-        <div v-for="hat in $options.hats">
+        <div v-for="hat in $options.hats" class="w-1/3 hover:w-2/3 rounded-lg">
             <i class="fas {{hat.icon}}"></i>
             <h2>{{hat.title}}</h2>
-            <ul v-for="item in hat.details">
-                
+            <ul v-for="item in hat.details" class>
+                <li>{{item}}</li>
             </ul>
         </div>
     </div>
