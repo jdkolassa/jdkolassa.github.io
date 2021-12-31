fa7d120d942e13f54002a0bf233fc51153b9d1c3
jdkolassa <jdkolassa@gmail.com>
2021-12-30 21:29:39 -0500

    Added background to description block so I can read it

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-    <div class="desc">
+    <div class="desc bg-pink-100">
         <h2 class="heading">I'M JEREMY KOLASSA</h2>
         <p class="text-center">Web developer, content curator, and project manager. At the intersection of computer and human languages is where I make my lair. I love translating technical concepts and jargon into plain English (or any other!) for clients and end-users. I've managed company-wide intiatives to overhaul workflows, written documentation to help users understand how interfaces work, and created eye-catching and pleasing front-end designs -- hooked up to efficient data-delivering backends. My goal is to reduce the number of migraines users, fellow developers, and stakeholders have.</p>
-        <p class="text-center">Because after the last few years, we could all use fewer headaches.</p>
+        <p class="text-center my-5">Because after the last few years, we could all use fewer headaches.</p>
     </div>
 </template>
