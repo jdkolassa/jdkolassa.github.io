bcae3b663a770af5766d8758eb46062f1721488f
jdkolassa <jdkolassa@gmail.com>
2021-12-30 21:28:59 -0500

    Added a 48rem width to the Tailwind config as the default options are tiny AF

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '192': '48rem'
+      }
+    },
     fontFamily: {
       'sans': ['Montserrat', 'Helvetica', 'sans-serif'],
       'heading': ['Gruppo', 'Impact', 'sans-serif']
