d6934a399b808fe4009bec3f81580a11008269b8
Jeremy Kolassa <jdkolassa@gmail.com>
2022-01-18 15:34:36 -0500

    Intro Logo work
    Cleaned up the intro logo, added animation to one text element to fade in

--- src/components/IntroLogo.vue
+++ src/components/IntroLogo.vue
@@ -1,24 +1,14 @@
 
 <template>
     <div id="introLogo" class="h-screen w-full max-w-[960px] flex-col mb-6">
-        <svg id="triangles" class=" mb-6 w-750 h-750">
+      <div id="artbox" class="contents">
+        <svg id="triangles" class=" mb-6 w-2/3 h-750 mx-auto">
            <path id="pinktri" fill="none" stroke="#F6019D" stroke-width="5" d="M250,50 L25,275 L675,550 Z" /> 
            <path id="cyantri" fill="none" stroke="#2DE2D6" stroke-width="5" d="M75,150 L175,550 L785,50 Z" />
         </svg>
         <br>
-        <div id="wd__container" class="">
-          <svg id="webDesign" width="488.6" height="168.04" version="1.1" viewBox="0 0 129.27 44.46" xmlns="http://www.w3.org/2000/svg">
-            <g transform="translate(-25.935 -104.84)">
-              <text transform="rotate(-15.073)" x="-8.3579836" y="148.06142" style="fill:#f6019d;font-family:Stingray;font-size:19.756px;line-height:1.25;stroke-width:.26458" xml:space="preserve"><tspan x="-8.3579836" y="148.06142" style="fill:#f6019d;stroke-width:.26458">Web Design</tspan></text>
-            </g>
-            <g transform="translate(-25.935 -104.84)">
-              <text transform="rotate(-15.073)" x="-9.0762415" y="147.70229" style="fill:#2de2d6;font-family:Stingray;font-size:19.756px;line-height:1.25;stroke-width:.26458" xml:space="preserve"><tspan x="-9.0762415" y="147.70229" style="fill:#2de2d6;stroke-width:.26458">Web Design</tspan></text>
-            </g>
-          </svg>
-        </div>
-
-        
-
+        <img id="webdesign" src="../assets/web_design_opt2.svg" class="absolute bottom-0 place-self-center">
+      </div>
     </div>
 
 </template>
@@ -56,6 +46,31 @@
   stroke-dasharray: 2083;
   stroke-dashoffset: 2083;  
   filter: drop-shadow(5px 5px 5px rgb(246,1,157, 0.75));
+}
+
+@keyframes draw {
+    from {
+      stroke-dashoffset: 1680;
+    }
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+
+#webdesign {
+  animation: fade-in 5s linear 6s;
+  animation-fill-mode: forwards;
+  opacity: 0;
+}
+
+@keyframes fade-in {
+  from {
+   opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
 }
 
 </style>
