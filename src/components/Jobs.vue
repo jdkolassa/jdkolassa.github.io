d3b8910746f647d0800de1b992002b357f37954b
Jeremy Kolassa <jdkolassa@gmail.com>
2022-01-19 15:32:57 -0500

    Trying to fix carousel
    Trying to get the carousel to look the right way, but it never does. Thinking of replacing the library with a different one.

--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -6,13 +6,13 @@ import { positions } from '../data/positions.json';
 </script>
 
 
-<template>
+<template #slides>
 
-<Carousel :itemsToShow="1.5" :wrapAround="true" class="w-full">
-    <Slide v-for="position in positions" :key="position" class="rounded-2xl bg-black text-white border-cyan-400 border border-double shadow-lg flex-col mx-1.5 p-2">
+<Carousel :itemsToShow="1" :wrapAround="true" :snapAlign="start">
+    <Slide v-for="position in positions" :key="position" class="job rounded-2xl text-white border-cyan-400 border border-double flex-col mx-2 p-2">
       <h1 class="heading">{{position.title}}</h1>
       <div>
-        <div class="jobcard border-2 border-red-900 text-center">
+        <div class="jobcard text-center">
             <img :src="'/src/assets/' + `${position.logo}` + '.jpeg'" class="mx-auto w-1/4">
             <h4 class="heading">{{position.employer}}</h4>
             <ul class="list-none list-outside">
@@ -37,10 +37,14 @@ import { positions } from '../data/positions.json';
 
 <style scoped>
 
-.details {
-  list-style-image: url("../assets/arrow_smol.png");
+.job {
+  background: rgba(146,0,117,0.15);
 }
-.carousel__slide > .carousel__item {
+
+.details {
+  list-style-image: url("../assets/point_24x24.png");
+}
+/* .carousel__slide > .carousel__item {
   transform: scale(1);
   opacity: 0.5;
   transition: 0.5s;
@@ -57,13 +61,18 @@ import { positions } from '../data/positions.json';
 }
 .carousel__slide--active > .carousel__item {
   transform: scale(1.1);
-}
+} */
 </style>
 
 
 
 <script>
 export default {
-  name: "Jobs"
+  name: "Jobs",
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+  },
 };
 </script>
