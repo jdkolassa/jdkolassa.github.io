ddfb7a1c0898616b7f6b2cb75dd6041cfc23b8ec
Jeremy Kolassa <jdkolassa@gmail.com>
2022-01-22 13:50:35 -0500

    Fixed error in deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A 
 git commit -m "deploy"
 
-git push -f git@github:jdkolassa/jdkolassa.github.io.git main
+git push -f git@github.com:jdkolassa/jdkolassa.github.io.git main
 
 cd -
