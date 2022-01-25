272529a83fc6b2b0feddcfae349fd681e222aeba
Jeremy Kolassa <jdkolassa@gmail.com>
2022-01-25 18:16:18 -0500

    Fixed Tailwind blooper
    Accidentally overrode the font-sizes instead of extending them. Fixed that!

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,19 @@ module.exports = {
     extend: {
       width: {
         '192': '48rem'
+      },
+      fontFamily: {
+        'sans': ['Montserrat', 'Helvetica', 'sans-serif'],
+        'heading': ['Gruppo', 'Impact', 'sans-serif']
+      },
+      fontSize: {
+        '10xl': '10rem',
+        '11xl': '12rem',
+        '12xl': '14rem',
+        '13xl': '16rem',
       }
     },
-    fontFamily: {
-      'sans': ['Montserrat', 'Helvetica', 'sans-serif'],
-      'heading': ['Gruppo', 'Impact', 'sans-serif']
-    },
-    fontSize: {
-      '10xl': '10rem',
-      '11xl': '12rem',
-      '12xl': '14rem',
-      '13xl': '16rem',
-    }
+    
   },
   plugins: [
     plugin(function({ addBase, theme }) {
