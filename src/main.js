94a38eb8b22cdbee95ac6fb3a8f06f767931ff61
Jeremy Kolassa <jdkolassa@gmail.com>
2022-01-25 11:51:56 -0500

    Fixed main.js to remove a change I made to placate GitHub

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import { createApp } from '../node_modules/vue/dist/vue.esm-browser.js'
-// import { createApp } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
