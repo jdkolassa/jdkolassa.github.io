03b5e661368c6bc39d29eb0bc64b229f950d7d37
Jeremy Kolassa <jdkolassa@gmail.com>
2022-11-27 17:08:15 -0500

    Jobs pre-refactor still requires Swiper, so it crashes. Removing it from App.vue on this branch.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs;
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup;
-import IntroLogo from './components/IntroLogo.vue';
-import Contact from './components/Contact.vue';
-import Description from './components/Description.vue';
-import Hats from './components/Hats.vue';
-import Jobs from './components/Jobs.vue';
+import IntroLogo from "./components/IntroLogo.vue";
+import Contact from "./components/Contact.vue";
+import Description from "./components/Description.vue";
+import Hats from "./components/Hats.vue";
+import Jobs from "./components/Jobs.vue";
 </script>
 
 <script>
-  export default {
-    name: "App",
-    components: {
-      Contact, Description, Hats, Jobs,
-    }
+export default {
+  name: "App",
+  components: {
+    Contact,
+    Description,
+    Hats,
+    Jobs,
+  },
 };
 </script>
 
@@ -21,7 +24,6 @@ import Jobs from './components/Jobs.vue';
   <IntroLogo></IntroLogo>
   <Description class="my-12"></Description>
   <Hats class="my-12"></Hats>
-  <Jobs class="my-12"></Jobs>
   <Contact class="my-12"></Contact>
 </template>
 
